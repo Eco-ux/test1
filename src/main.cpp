7157c5526707ef568a5f2b6fdccbaaa5d98d70a8
Eco <you@example.com>
2025-10-03 06:40:46 +0700

    Use OneButton: single=ON/OFF, double=blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
 #include <Arduino.h>
+#include <OneButton.h>
+
+#define LED_PIN 5
+#define BTN_PIN 4
+
+OneButton button(BTN_PIN, true);
+
+bool ledState = false;
+bool blinkMode = false;
+
+void toggleLED() {
+  if (!blinkMode) {
+    ledState = !ledState;
+    digitalWrite(LED_PIN, ledState);
+  }
+}
+
+void startBlink() {
+  blinkMode = !blinkMode;
+}
 
 void setup() {
-  pinMode(5, OUTPUT);
-
+  pinMode(LED_PIN, OUTPUT);
+  button.attachClick(toggleLED);        // single click
+  button.attachDoubleClick(startBlink); // double click
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(5, 0);
-  delay(500);
-  digitalWrite(5,1);
-  delay(500);
-
+  button.tick();
+  if (blinkMode) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(200);
+    digitalWrite(LED_PIN, LOW);
+    delay(200);
+  }
 }
